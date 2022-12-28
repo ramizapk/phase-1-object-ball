f41edb217ab179545ac9716256735b5ae6758f9b
Valencia <starrv2011@gmail.com>
2022-12-27 19:33:52 -0500

    working on creating objects

---
+++ src/00-objectball.js
@@ -0,0 +1,20 @@
+function gameObject(){
+    const gameObj={};
+    let colors=["black","white"];
+    let players=["Alan Anderson","Reggie Evans","Brook Lopez","Mason Plumlee","Jason Terry"];
+    gameObj.home=teamObject("Brooklyn Nets",colors,playersObject(players));
+    colors=["Turquoise","Purple"];
+    players=["Jeff Adrien","Bismak Biyombo","DeSagna Diop","Ben Gordon","Brendan Haywood"];
+    gameObj.away=teamObject("Charlotte Gardens",colors,playersObject(players));
+
+    function teamObject(name,colors,players){
+
+    }
+    
+    function playersObject(players){
+        
+    }
+    
+
+}
+
